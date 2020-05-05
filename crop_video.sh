8065dc48436385868a0ee4eadd54748632a27452
François Trahay <francois.trahay@telecom-sudparis.eu>
2020-05-05 14:28:00 +0200

    crop_video: add the possibility to show only the main screen (and to remove the webcam)

--- crop_video.sh
+++ crop_video.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
 
+usage()
+{
+cat << EOF
+usage: $0 [options] input_file output_file
+
+OPTIONS:
+   -?                               Show this message
+   -s startup_duration              Remove the first startup_duration seconds of the video
+   -m   	       	            Only show the main screen (ie. remove the webcam)
+EOF
+
+}
 
 if [ $# -lt 2 ]; then
     echo "Usage: $0 input_file output_file [startup_duration]" >&2
     exit 1
 fi
 
+startup_duration=11 # duration of firefox startup (that will be cut out of the video)
+main_screen_only=n
+
+while getopts 's:m' OPTION; do
+    case $OPTION in
+	s)
+	    startup_duration=$OPTARG
+	    ;;
+	m)
+	    main_screen_only=y
+	    ;;
+	  ?)	usage
+	exit 2
+	;;
+    esac
+done
+
+# remove the options from the command line
+shift $(($OPTIND - 1))
+
 input=$1
 output=$2
-startup_duration=11 # duration of firefox startup (that will be cut out of the video)
-
-if [ $# -eq 3 ]; then
-    startup_duration=$3
-fi
- 
 
 video_size=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 $input )
 height=$(echo $video_size |awk -Fx '{print $2}')
@@ -26,6 +52,10 @@ lower_window=56 # height of the lower part of the firefox window
 
 #out_w is the width of the output rectangle
 out_w=$width
+if [ "$main_screen_only" = "y" ]; then
+    out_w=720
+fi
+
 #out_h is the height of the output rectangle
 out_h=$(echo "$height - $upper_window - $lower_window"|bc)
 
