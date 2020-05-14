0b3ab4dda57c2a1a333bbd31386b71945de7d72b
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2020-05-14 22:17:03 +0200

    Add progress bar on the downloading of videos
    requires python3-progressbar on my Debian
    
    Also handle the case where only slides were displayed and no
    screensharing was recorded

--- download_bbb_data.py
+++ download_bbb_data.py
@@ -5,6 +5,30 @@ import sys
 import os
 import bbb
 import requests
+import time
+import numpy as np
+import progressbar
+
+
+# Borrowed from https://stackoverflow.com/a/37574635 adjusting the buffer size to 10Mb
+def download_file(url, filename, n_chunk=1):
+    r = requests.get(url, stream=True)
+    # Estimates the number of bar updates
+    block_size = 10*1024*1024
+    content_length = r.headers.get('Content-Length', None)
+    if r.status_code != 404 :
+        file_size = int(content_length)
+        num_bars = np.ceil(file_size / (n_chunk * block_size))
+        bar =  progressbar.ProgressBar(maxval=num_bars).start()
+        with open(filename, 'wb') as f:
+            for i, chunk in enumerate(r.iter_content(chunk_size=n_chunk * block_size)):
+                f.write(chunk)
+                bar.update(i+1)
+                # Add a little sleep so you can see the bar progress
+                time.sleep(0.05)
+        print("")
+    return r.status_code
+
 
 if __name__ == '__main__' :
     url=sys.argv[1]
@@ -53,9 +77,12 @@ if __name__ == '__main__' :
     i=1
     os.makedirs(output_videos_dir, exist_ok=True)
     for formats in extractor.formats:
-        print("["+str(i)+"/"+str(len(extractor.formats))+"]  "+formats['format_id']+ " ("+formats['url']+")")
-        i=i+1
-
         video_name=output_videos_dir+formats['url'].split('/')[-1]
-        r = requests.get(formats['url'])
-        open(video_name , 'wb').write(r.content)
+
+        status_code = download_file(formats['url'], video_name)
+        if status_code != 404 :
+            print("["+str(i)+"/"+str(len(extractor.formats))+"]  saved '"+formats['format_id']+ "' ("+formats['url']+").")
+        else :
+            print("["+str(i)+"/"+str(len(extractor.formats))+"]  no '" + formats['format_id'] + "' recording could be found at " + formats['url'] + " (" + str(status_code) + ").")
+
+        i += 1
